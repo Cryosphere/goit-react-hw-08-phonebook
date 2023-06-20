53c90054d5d599fe8512b4b6c9b251ab84b49722
Sergey Shmatok <trycifer@gmail.com>
2023-06-20 21:15:07 +0300

    Add styles

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,6 +1,64 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://64801fd4f061e6ec4d489cbd.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+const setAuthHeader = token =>
+  (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+const cleanAuthHeader = () =>
+  (axios.defaults.headers.common.Authorization = '');
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/users/signup', user);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const loginization = createAsyncThunk(
+  'auth/loginization',
+  async (user, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/users/login', user);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(
+  'auth/logout',
+  async (user, { rejectWithValue }) => {
+    try {
+      await axios.post('/users/logout', user);
+      cleanAuthHeader();
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  'auth/refreshUser',
+  async (_, { getState, rejectWithValue }) => {
+    const { token } = getState().auth;
+    token && setAuthHeader(token);
+
+    try {
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filterSlice.js';
 import { contactsReducer } from './contactSlice.js';
+import { authReducer } from './authSlice.js';
+
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    auth: persistReducer(persistConfig, authReducer),
     filter: filterReducer,
+    contacts: contactsReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
