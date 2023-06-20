cd73f9a18b436f619440b46f7b852d6d8ff3a424
Sergey Shmatok <trycifer@gmail.com>
2023-06-20 21:50:15 +0300

    Reset

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,93 +2,27 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import style from './App.module.css';
-import { useEffect, lazy } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectError, selectIsLoading } from '../redux/selectors';
-import { fetchContacts } from '../redux/operations';
-
-import { GlobalStyle } from './GlobalStyle';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { Layout } from './Layout/Layout';
-import { refreshUser } from '../redux/operations'; // для обновления токена
-import { PrivateRoute } from './PrivateRoute'; // для захисту роутів
-import { RestrictedRoute } from './RestrictedRoute'; // для захисту роутів
-import { ToastContainer } from 'react-toastify'; // для вспливаючих повідомлень
-import { Spiner } from 'pages/ContactList/ContactList.styled';
-
-const Register = lazy(() => import('../pages/Register/Register'));
-const Home = lazy(() => import('../pages/Home/Home'));
-const Login = lazy(() => import('../pages/Login/Login'));
-const Contactlist = lazy(() => import('../pages/ContactList/ContactList'));
-
-// export const App = () => {
-//   const dispatch = useDispatch();
-//   const isLoading = useSelector(selectIsLoading);
-//   const error = useSelector(selectError);
-
-//   useEffect(() => {
-//     dispatch(fetchContacts());
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       <h1 className={style.header}>Phonebook</h1>
-//       <ContactForm />
-//       <Filter />
-//       {isLoading && !error && <b>Request in progress...</b>}
-//       <ContactList />
-//     </div>
-//   );
-// };
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
-    dispatch(refreshUser()); // для обновления токена
+    dispatch(fetchContacts());
   }, [dispatch]);
 
-  const { isRefreshing } = useSelector(state => state.auth);
-
-  // якщо не обновляється токен, то рендеримо компоненти
-  return !isRefreshing ? (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute component={<Contactlist />} redirectTo="/login" />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<Register />}
-              />
-            }
-          />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-
-      {/* для вспливаючих повідомлень */}
-      <ToastContainer />
-
-      {/* для глобальних стилів */}
-      <GlobalStyle />
-    </>
-  ) : (
-    <Spiner />
+  return (
+    <div>
+      <h1 className={style.header}>Phonebook</h1>
+      <ContactForm />
+      <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactList />
+    </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filterSlice.js';
 import { contactsReducer } from './contactSlice.js';
-import { authReducer } from './authSlice.js';
-
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
-const persistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
 
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(persistConfig, authReducer),
-    filter: filterReducer,
     contacts: contactsReducer,
+    filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
